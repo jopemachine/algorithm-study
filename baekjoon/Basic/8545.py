41da8225985d1c60c110842820d2c9dfdad18f2a
Gyubong <jopemachine@naver.com>
2023-01-01 01:57:31 +0900

    [Bronze 5] Solve 8545, Zadanie próbne https://www.acmicpc.net/problem/8545

---
+++ baekjoon/Basic/8545.py
@@ -0,0 +1,17 @@
+'''
+==============================================================================================
+@ Title: Zadanie próbne
+@ URL: https://www.acmicpc.net/problem/8545
+@ Author: jopemachine
+@ Created Date: 1/1/2023, 1:55:10 AM
+@ Tags: implementation string
+@ Level: Bronze 5
+@ Description:
+Napisz program, który odwraca podane słowo trzyliterowe.
+@ Input: W pierwszym i jedynym wierszu podane jest jedno słowo trzyliterowe.
+@ Output: Pierwszy i jedyny wiersz wyjścia powinien zawierać odwrócone słowo
+wejściowe.
+==============================================================================================
+'''
+
+print(input()[::-1])
